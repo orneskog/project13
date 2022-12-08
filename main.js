659d0ea4afce6f99a9270885cdc7aca0c05b1f58
Charlie Örneskog <charlie.orneskog@gmail.com>
2022-12-08 02:49:45 +0100

    Har gjort kaos med allt hej

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
-window.onload = function() {
-    let orderButton = document.getElementById("order-button");
-}
-function updateCollectedItems() {
-    if(sessionStorage.getItem("orderButton") !== null){
-        console.log("true")
-        cup.classList.replace("basketheader", "basketheader-item");
-    }
-}
 // Function to open hamburger menu
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
@@ -21,16 +12,4 @@ document.querySelectorAll(".nav-link").forEach(n = n.
     addEventListener("click", () => {
         hamburger.classList.remove("active");
         navMenu.classList.remove("active");
-    }))
-
-// Order function to change basket picture to 1 item
-orderButton.addEventListener("click", () => {
-            saveToSessionStorage("orderButton", true);
-            console.log("test")
-            updateCollectedItems();
-         });
-
-//Function to save our keys and values to the sessionStorage.
-function saveToSessionStorage(key, value) {
-    sessionStorage.setItem(key, value);
-}
+}))
---
+++ package-items.js
@@ -0,0 +1,55 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const selected = document.querySelector('.selected');
+const options = document.querySelector('.options');
+const optionList = document.querySelectorAll('.option');
+const selected2 = document.querySelector('.selected2');
+const options2 = document.querySelector('.options2');
+const optionList2 = document.querySelectorAll('.option2');
+const selected3 = document.querySelector('.selected3')
+const options3 = document.querySelector('.options3')
+const optionList3 = document.querySelectorAll('.option3')
+
+selected.addEventListener('click', () => {
+    options.classList.toggle('active')
+})
+selected2.addEventListener('click', () => {
+    options2.classList.toggle('active')
+})
+selected3.addEventListener('click', () => {
+    options3.classList.toggle('active')
+})
+
+optionList.forEach(option => {
+    option.addEventListener('click', () => {
+        selected.innerHTML = option.querySelector('span').innerText
+         options.classList.remove("active")
+    })
+})
+
+optionList2.forEach(option2 => {
+    option2.addEventListener('click', () => {
+        selected2.innerHTML = option2.querySelector('span').innerText
+         options2.classList.remove("active")
+    })
+})
+
+optionList3.forEach(option3 => {
+    option3.addEventListener('click', () => {
+        selected3.innerHTML = option3.querySelector('span').innerText
+         options3.classList.remove("active")
+    })
+})
+
+// Function to open hamburger menu
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+// Function to close hamburger menu
+document.querySelectorAll(".nav-link").forEach(n = n.
+    addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+}))
